2e540ea66a95a452b4b6ca6e2d76955c86d280e2
peppera629 <peppera629@yahoo.com>
2026-01-02 18:09:07 +0800

    Removed unnecessary files

--- cookierunTCG/src/module-info.java
+++ cookierunTCG/src/module-info.java
@@ -6,6 +6,9 @@
  */
 module cookierunTCG {
     requires transitive java.desktop;
+    //requires transitive javafx.controls;
+    //requires transitive javafx.graphics;
+    //requires transitive javafx.fxml;
     exports ui;
     exports dataStructure;
     exports util;
